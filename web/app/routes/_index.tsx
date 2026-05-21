b70af34881e2b1de924cbd87e44adb075eb4b278
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-05-21 11:14:20 +0530

    recommen

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -4,9 +4,13 @@ import { login } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
-  if (url.searchParams.get("shop")) {
-    throw redirect(`/auth?${url.searchParams.toString()}`);
+  const shop = url.searchParams.get("shop");
+
+  if (shop) {
+    // Pass all params to /app where authentication is handled
+    return redirect(`/app?${url.searchParams.toString()}`);
   }
+
   return login(request);
 };
 
